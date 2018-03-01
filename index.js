c69084d4813dbe7dad51bf3db301f43e8feaf4bc
Brandon Kim <bkim95ca@gmail.com>
2018-03-01 00:29:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 var obj = {prop: 1}
 
-return updateObjectWithKeyAndValue({}, obj, 'prop2', '2')
+return Object.assign({}, obj, 'prop2', '2')
 
 }
