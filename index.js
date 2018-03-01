f7c3dc72fbb61d678eb10419782241e9cac70bcd
Brandon Kim <bkim95ca@gmail.com>
2018-03-01 00:57:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
  const newObj = Object.assign({}, object)
 
+delete newObj[key]
+
  return newObj
 }
